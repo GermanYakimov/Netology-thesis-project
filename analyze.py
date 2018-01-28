27a25541326f80f3bfb9c116d1a2421494b5f07c
German Yakimov <german13yakimov@gmail.com>
2018-01-28 18:09:01 +0600

    Улучшение алгоритма поиска уникальных групп (убран лишний флаг), удаление лишних комментариев

--- analyze.py
+++ analyze.py
@@ -3,14 +3,11 @@ import time
 
 
 def find_unique_communities(user_communities, user_friends_communities):
-    unique_groups = []  # За уникальные так же считаем и такие группы, в которых состоят до 3 друзей пользователя
+    unique_groups = []
 
     with progressbar.ProgressBar(max_value=len(user_communities)) as bar:
         for i, group in enumerate(user_communities):
-            friends_in_community = False  # Флаг, отражающий то, состоят ли друзья в сообществе
-            if group in user_friends_communities:
-                friends_in_community = True
-            if not friends_in_community:
+            if group not in user_friends_communities:
                 unique_groups.append(group)
             bar.update(i)
             time.sleep(0.2)
